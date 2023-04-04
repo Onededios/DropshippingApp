118d56a699ac1fef5018a491168e25624e1289e0
Onededios <joel10olor@gmail.com>
2023-04-04 16:13:25 +0200

    Added JAVADOC to Docs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Client"},{"p":"<Unnamed>","l":"Controller"},{"p":"<Unnamed>","l":"EditConsole"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Menu"},{"p":"<Unnamed>","l":"Order"},{"p":"<Unnamed>","l":"Product"},{"p":"<Unnamed>","l":"Read"},{"p":"<Unnamed>","l":"Vendor"}]
